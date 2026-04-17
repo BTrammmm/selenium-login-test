a21382990d01a009a2a75db3fedc0e7cb367d7b7
phambichtram <167198204+BTrammmm@users.noreply.github.com>
2026-04-17 22:51:44 +0700

    Update driver_setup.py

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
     return driver
