b107ca3994a744b24a7a475c748d87f44b288297
phambichtram <167198204+BTrammmm@users.noreply.github.com>
2026-04-17 22:52:16 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,11 +4,13 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    username = (By.ID, "username")
-    password = (By.ID, "password")
-    login_btn = (By.ID, "login")
+    def open(self):
+        self.driver.get("https://the-internet.herokuapp.com/login")
 
-    def login(self, user, pwd):
-        self.driver.find_element(*self.username).send_keys(user)
-        self.driver.find_element(*self.password).send_keys(pwd)
-        self.driver.find_element(*self.login_btn).click()
+    def login(self, username, password):
+        self.driver.find_element(By.ID, "username").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    def get_message(self):
+        return self.driver.find_element(By.ID, "flash").text
