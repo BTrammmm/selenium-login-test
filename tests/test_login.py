e64b55712ae905355efc66bd9c8be677db1f4218
phambichtram <167198204+BTrammmm@users.noreply.github.com>
2026-04-17 22:37:45 +0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,7 @@
-import json
 from pages.login_page import LoginPage
 
-def load_data():
-    with open("data/test_data.json") as f:
-        return json.load(f)
+def test_login(driver):
+    driver.get("https://example.com/login")
 
-def test_login_success(driver):
-    data = load_data()
-    user = data["valid_user"]
-
-    page = LoginPage(driver)
-    page.open()
-    page.login(user["username"], user["password"])
-
-    assert "secure" in driver.current_url
-
-
-def test_login_fail(driver):
-    data = load_data()
-    user = data["invalid_user"]
-
-    page = LoginPage(driver)
-    page.open()
-    page.login(user["username"], user["password"])
-
-    assert "error" in driver.page_source
+    login = LoginPage(driver)
+    login.login("admin", "123456")
