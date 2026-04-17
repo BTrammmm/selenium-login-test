486a35f9c3a4e8a6e45baf925931fecb0bfa75b3
phambichtram <167198204+BTrammmm@users.noreply.github.com>
2026-04-17 22:37:05 +0700

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,14 @@
 from selenium.webdriver.common.by import By
-from utils.config import BASE_URL
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def open(self):
-        self.driver.get(BASE_URL)
+    username = (By.ID, "username")
+    password = (By.ID, "password")
+    login_btn = (By.ID, "login")
 
-    def login(self, username, password):
-        self.driver.find_element(By.ID, "username").send_keys(username)
-        self.driver.find_element(By.ID, "password").send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    def login(self, user, pwd):
+        self.driver.find_element(*self.username).send_keys(user)
+        self.driver.find_element(*self.password).send_keys(pwd)
+        self.driver.find_element(*self.login_btn).click()
